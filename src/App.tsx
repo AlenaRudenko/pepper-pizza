2f9b2608140bbc7146d2e4b64874b5bf9b746cd6
AlenaRudenko <pineapple001@mail.ru>
2022-02-09 11:38:45 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "./pages/main-page/header/Header";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='app__container'>
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
