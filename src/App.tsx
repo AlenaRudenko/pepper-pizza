a0be10a6dd1637fa08202f8ed87716504409cd3f
Alyona Rudenko <alenka-rudenko@mail.ru>
2022-09-02 11:02:32 +0000

    Update App.tsx and Main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import React, { useState } from 'react'
+
 import { Main } from './Main'
 
-export interface IContext {
-    colors: IColors;
-    setColors: () => void;
-}
-export interface IColors {
-    backgroundCApp:string;
-    backgroundCAppContainer:string;
-}
-export const Context = React.createContext<IContext>({colors:{backgroundCApp:'#ffdf8c',
-backgroundCAppContainer:'white'}, setColors: () => {}})
-export const App = () => {
-    const [colors, setColors] = useState({
-        backgroundCApp:'#ffdf8c',
-        backgroundCAppContainer:'white'
 
-    });
-    const setColorEditor = () => {
-        setColors((prevState) => ({...prevState,backgroundCApp: colors.backgroundCApp === '#ffdf8c' ? '1f304e' : '#ffdf8c' }))
-    }
-    const context:IContext = {colors,setColors:setColorEditor};
-    return <Context.Provider value={context}><div style={{backgroundColor:colors.backgroundCApp}}className='app'><Main /></div>
-        </Context.Provider>
-        
+export const App = () => {
+    return <div className='app'><Main /></div>       
 }
